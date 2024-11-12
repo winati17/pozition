4be7254c48f9ce4ea9122f46fdb912b26aaa7137
Mirsa <mirsabintang0@gmail.com>
2024-11-13 00:09:07 +0800

    feat: implement routing to OrderPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import LandingPage from './pages/LandingPage';
+import OrderPage from './pages/OrderPage';
 
 import '@ionic/react/css/core.css';
 
@@ -14,6 +15,8 @@ const App = () => (
       <IonRouterOutlet>
         <Route exact path="/landing" component={LandingPage} />
         <Redirect exact from="/" to="/landing" />
+        <Route exact path="/order" component={OrderPage} />
+        <Redirect exact from="/" to="/order" />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
