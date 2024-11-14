d2be073e5ccf345914f4fe499456ef84356da4e4
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2024-11-14 15:29:04 +0800

    set page redirection after login/signup

--- OneDrive/others/Desktop/fathin/PROJECT/sisfor-ionic-v2/myApp/src/App.tsx
+++ OneDrive/others/Desktop/fathin/PROJECT/sisfor-ionic-v2/myApp/src/App.tsx
@@ -1,5 +1,10 @@
 import { Redirect, Route } from "react-router-dom";
-import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import {
+  IonApp,
+  IonRouterOutlet,
+  IonSpinner,
+  setupIonicReact,
+} from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -34,20 +39,41 @@ import "@ionic/react/css/palettes/dark.system.css";
 
 /* Theme variables */
 import "./theme/variables.css";
+import Example from "./pages/Example";
+import React, { useEffect, useState } from "react";
+import { getCurrentUser } from "./firebaseConfig";
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
+const RoutingSystem: React.FC = () => {
+  return (
     <IonReactRouter>
       <IonRouterOutlet>
         <Route path="/home" component={Home} exact={true} />
         <Route path="/login" component={Login} exact={true} />
         <Route path="/signup" component={SignUp} exact={true} />
+        <Route path="/example" component={Example} exact={true} />
         <Redirect exact from="/" to="/home" />
       </IonRouterOutlet>
     </IonReactRouter>
-  </IonApp>
-);
+  );
+};
+
+const App: React.FC = () => {
+  const [busy, setBusy] = useState(true);
+
+  useEffect(() => {
+    getCurrentUser().then((user) => {
+      if (user) {
+        //user logged in
+        window.history.replaceState({}, "", "./Example");
+      } else {
+        window.history.replaceState({}, "", "./");
+      }
+    });
+  }, []);
+
+  return <IonApp>{busy ? <IonSpinner /> : <RoutingSystem />}</IonApp>;
+};
 
 export default App;
