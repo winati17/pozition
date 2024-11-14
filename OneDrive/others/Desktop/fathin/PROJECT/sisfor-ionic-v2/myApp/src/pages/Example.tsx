7e106ab326b3ad836830bc61208324371cd56a88
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2024-11-14 14:50:07 +0800

    change Example.tsx

--- OneDrive/others/Desktop/fathin/PROJECT/sisfor-ionic-v2/myApp/src/pages/Example.tsx
+++ OneDrive/others/Desktop/fathin/PROJECT/sisfor-ionic-v2/myApp/src/pages/Example.tsx
@@ -6,17 +6,8 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
-import "./Home.css";
-import { useEffect, useState } from "react";
 
-const Home: React.FC = () => {
-  const [input, setInput] = useState<string>("");
-
-  useEffect(() => {
-    console.log(input);
-  }, [input]);
-
+const Example: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
@@ -31,4 +22,4 @@ const Home: React.FC = () => {
   );
 };
 
-export default Home;
+export default Example;
