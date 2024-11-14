7a521dfbb1c7b20569f876a047581526b18af917
Rifat <rifat.khaidir@gmail.com>
2024-11-14 16:33:25 +0800

    DetailVenue

---
+++ src/components/icons.js
@@ -0,0 +1,11 @@
+import parkirIcon from "./parkir_icon.png";
+import wifiIcon from "./wifi_icon.png";
+import toiletIcon from "./toilet_icon.png";
+import restoIcon from "./resto_icon.png";
+
+export const icons = {
+  parkir: parkirIcon,
+  wifi: wifiIcon,
+  toilet: toiletIcon,
+  resto: restoIcon,
+};
